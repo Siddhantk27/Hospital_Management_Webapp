ad9c5453ae4d6db7e0fae90eb7719d914a48db8a
Siddhantk27 <135336641+Siddhantk27@users.noreply.github.com>
2024-04-06 18:42:15 +0530

    adding files

---
+++ Hospital_Management_Webapp-main/backend/database/dbConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+exports.dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log("Some error occurred while connecting to database:", err);
+    });
+};
